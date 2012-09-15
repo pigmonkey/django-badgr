479abd2513331cb467b399577aae329c08875352
Pig Monkey <pm@pig-monkey.com>
2012-09-15 14:13:01 -0700

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-badgr',
-    version = '0.1',
-    description = 'A resuable Django application to display a Flickr badge.',
-    long_description = open('README.md').read(),
-    url = 'https://github.com/pigmonkey/django-badgr',
-    author = 'Pig Monkey',
-    author_email = 'pm@pig-monkey.com',
-
-    packages = find_packages(),
+    name='django-badgr',
+    packages=find_packages(),
+    version='1.0',
+    description='A resuable Django application used to display a Flickr badge.',
+    author='Peter Hogg',
+    author_email='peter@havenaut.net',
+    url='https://github.com/pigmonkey/django-badgr',
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+    ],
+    long_description=open('README.md').read(),
+    include_package_data=True,
     zip_safe=False,
+    install_requires=['flickrapi'],
 )
